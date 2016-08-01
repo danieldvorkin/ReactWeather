7f3aa5ffd4f1b55c141c2bbdb79f8524cb65e054
Zaki Khan <zakmysta@gmail.com>
2016-07-31 22:45:40 -0400

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather app built on <a href="https://facebook.github.io/react/">React</a> and <a href="http://openweathermap.org/">OpenWeatherMap</a>.
+      </p>
+      <p>
+        <a href="https://github.com/zakmysta/ReactWeather">Github source</a> for app.
+      </p>
     </div>
   );
 };
