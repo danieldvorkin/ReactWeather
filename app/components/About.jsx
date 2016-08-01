6bf2833b6eb65382c5375c2212f4ea56fe17df01
Zaki Khan <zakmysta@gmail.com>
2016-08-01 00:52:45 -0400

    Add app.css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         This is a weather app built on <a href="https://facebook.github.io/react/">React</a> and <a href="http://openweathermap.org/">OpenWeatherMap</a>.
       </p>
